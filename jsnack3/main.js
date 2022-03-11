36b54babe384809a6855c1e1e928e31ed9698f69
Gioia Ascari <gioia.ascari96@gmail.com>
2022-03-11 18:12:57 +0100

    creo due array con numeri diversi

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,3 +1,7 @@
 console.log("Ok Js");
 //Crea due array che hanno un numero di elementi diversi.
 //Aggiungi elementi casuali all’array che ha meno elementi, fino a quando ne avrà tanti quanti l’altro.
+
+let arrayOne = [1, 2, 3, 4, 5];
+
+let arrayTwo = [6, 7, 8];
