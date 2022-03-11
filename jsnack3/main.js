e1b03ff034bddb2a63c58ccf4e31896837da2928
Gioia Ascari <gioia.ascari96@gmail.com>
2022-03-11 19:33:44 +0100

    esercizio completato con aggiunta di commenti

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,14 +1,16 @@
 console.log("Ok Js");
+alert("Benvenuto:)");
 //Crea due array che hanno un numero di elementi diversi.
 //Aggiungi elementi casuali all’array che ha meno elementi, fino a quando ne avrà tanti quanti l’altro.
 
+//CREO ARRAY CON DIVERSI ELEMENTI
 let arrayOne = [1, 2, 3, 4, 5];
-
 let arrayTwo = [6, 7, 8];
-
-while (arrayOne > arrayTwo) {
-  arrayTwo.push(Math.floor(Math.random()));
+//AGGIUNGO ELEMNTI CASUALI AD "ARRAYTWO"
+while (arrayOne.length > arrayTwo.length) {
+  //UTILIZZO "PUSH" PER AGGIUNERE ELEMENTI ED INFINE "MATH RANDOM" PER GENERARE NUMERI RANDOM
+  arrayTwo.push(arrayOne[Math.floor(Math.random() * arrayTwo.length)]);
 }
-
+//CONSOLE LOG RISULTATO
 console.log(arrayOne);
 console.log(arrayTwo);
