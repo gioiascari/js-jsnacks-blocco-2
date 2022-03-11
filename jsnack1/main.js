1f8a4447790c7b774c51bb3b84a1ff9bea630be0
Gioia Ascari <gioia.ascari96@gmail.com>
2022-03-11 14:54:12 +0100

    versione con for riuscita e quindi completata

--- jsnack1/main.js
+++ jsnack1/main.js
@@ -3,3 +3,12 @@ console.log("Ok Js");
 //Il software deve chiedere per 5 volte all’utente di inserire un numero.
 //Il programma stampa la somma di tutti i numeri inseriti.
 //Esegui questo programma in due versioni, con il for e con il while.
+
+//Versione con for
+let somma = 0;
+
+for (let i = 0; i < 5; i++) {
+  const numero = parseInt(prompt("Inserisci un numero"));
+  somma += numero;
+}
+console.log(somma);
