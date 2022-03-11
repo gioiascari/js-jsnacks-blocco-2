a8388a5c1e0ce6473f6b77b93e45340482974be8
Gioia Ascari <gioia.ascari96@gmail.com>
2022-03-11 14:36:38 +0100

    somma dei numeri dispari

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -1,1 +1,12 @@
 console.log("Ok Js");
+
+//Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let somma = 0;
+for (let i = 0; i < array.length; i++) {
+  if (array[i] % 2 !== 0) {
+    somma = somma + array[i];
+  }
+}
+console.log("La somma dei numeri dispari è " + somma);
